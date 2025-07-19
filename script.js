8a432a410f7b2b1f293310ebc1a74ed0ed816060
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2025-07-19 08:48:33 +0200

    feat: draw and display first card

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.addEventListener("load",function(){
 
+    let drawBtn = this.document.querySelector("#draw-button");
     let cardView = this.document.querySelector("#card-view");
     cardView.innerHTML="<h2>La carte s'affichera ici</h2>";
 
@@ -13,6 +14,12 @@ window.addEventListener("load",function(){
         .then(cards => {
             console.log('Cartes chargées :', cards);
             console.log('Nombre de cartes :', cards.length);
+
+            drawBtn.addEventListener("click", ()=>{
+                console.log("carte tirée !");
+                cardView.innerHTML="<img src='./"+cards[0].imgUrl+"'/>";
+                
+            });
         })
     .catch(error => console.error(error));
 })
