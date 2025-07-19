e6e0c70a5a96c0913161bd09fa0b742cc2f4835f
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2025-07-19 08:56:37 +0200

    feat: draw random card from the deck

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 window.addEventListener("load",function(){
 
+    // Le bouton
     let drawBtn = this.document.querySelector("#draw-button");
+    // L'affichage de la carte
     let cardView = this.document.querySelector("#card-view");
+    // Si aucune carte n'est tirée, afficher un tapis ou autre ...
     cardView.innerHTML="<h2>La carte s'affichera ici</h2>";
 
-    // Charger les cartes
+    // CHARGER LES CARTES
     fetch('./cards.json')
         .then(response => {
             if (!response.ok) throw new Error('Erreur de chargement');
@@ -15,11 +18,27 @@ window.addEventListener("load",function(){
             console.log('Cartes chargées :', cards);
             console.log('Nombre de cartes :', cards.length);
 
+            // TIRAGE ALÉATOIRE:
+            // Au clic sur le bouton,
             drawBtn.addEventListener("click", ()=>{
-                console.log("carte tirée !");
-                cardView.innerHTML="<img src='./"+cards[0].imgUrl+"'/>";
-                
+                // appeler drawRandomCard pour sortir une carte aléatoire
+                let randomCardImgUrl = drawRandomCard(cards);
+                // et afficher la carte
+                cardView.innerHTML="<img src='./"+randomCardImgUrl+"'/>";
             });
         })
     .catch(error => console.error(error));
 })
+
+// FONCTION DE TIRAGE ALÉATOIRE DANS LE DECK
+function drawRandomCard(cardsArray){
+
+    //Générer un index aléatoire dans les limites du nombre de cartes
+    let nbOfCards = cardsArray.length;
+    let randomIndex = Math.floor(Math.random() * nbOfCards);
+    console.log("randomIndex :", randomIndex);
+
+    //Récupérer l'url de l'image de la carte tirée
+    let randomCardImgUrl = cardsArray[randomIndex].imgUrl
+    return randomCardImgUrl;
+}
