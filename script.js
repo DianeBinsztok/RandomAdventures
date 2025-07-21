7f04fff0fff918aae4b027f3ef958e7725550919
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2025-07-21 17:16:34 +0200

    feat: draw random card with the spacebar

--- script.js
+++ script.js
@@ -23,56 +23,72 @@ window.addEventListener("load",function(){
             drawBtn.addEventListener("click", ()=>{
 
                 // Appeler drawRandomCard pour sortir une carte aléatoire                
-                let newRandomCardUrl = drawRandomCardNoDouble(cards);
+                let newRandomCard = drawRandomCardThenDiscardIt(cards);
 
                 // S'il reste des cartes dans le deck, afficher une carte aléatoire
-                if(newRandomCardUrl){
-                    cardView.innerHTML="<img src='./"+newRandomCardUrl.imgUrl+"'/>";
+                if(newRandomCard){
+                    cardView.innerHTML="<img src='./"+newRandomCard.imgUrl+"'/>";
                 }else{
                     cardView.innerHTML="<p>Votre deck est vide !</p>";
+                }
+                // Retirer le focus du bouton pour éviter son déclenchement au spacebar
+                drawBtn.blur();
+            });
+
+            // Au clic sur la barre d'espace
+            addEventListener("keydown", (event)=>{
+
+                // Vérifier que c'est bien la barre d'espace qui est pressée
+                if (event.key == " " || event.code == "Space") {
+
+                    // Éviter le scrolldown (par défaut quand on presse 'espace')
+                    event.preventDefault();
+
+                    // Appeler drawRandomCard pour sortir une carte aléatoire                
+                    let newRandomCard = drawRandomCardThenDiscardIt(cards);
+                    
+                    // S'il reste des cartes dans le deck, afficher une carte aléatoire
+                    if(newRandomCard){
+                        cardView.innerHTML="<img src='./"+newRandomCard.imgUrl+"'/>";
+                    }else{
+                        cardView.innerHTML="<p>Votre deck est vide !</p>";
+                    }                
                 }
             });
         })
     .catch(error => console.error(error));
 })
 
-// FONCTION DE TIRAGE ALÉATOIRE DANS LE DECK
-function drawRandomCard(cardsArray, currentIndex){
+// FONCTIONS
+// Tirage aléatoire au clic sur le bouton
+function drawRandomCardThenDiscardIt(cardsArray){
 
-    //Générer un index aléatoire dans les limites du nombre de cartes
-    let nbOfCards = cardsArray.length;
-    let randomIndex = Math.floor(Math.random() * nbOfCards);
-    console.log("randomIndex :", randomIndex);
-
-    // Si le nouvel index est le même que celui de la carte courante,
-    if(randomIndex == currentIndex){
-        console.log("Doublon ! Je tire une autre carte");
-        // refaire un tirage
-        return drawRandomCard(cardsArray, currentIndex);
-    }else{
-        // Renvoyer le nouvel index et l'url de la carte
-        return {"newIndex":randomIndex, "imgUrl":cardsArray[randomIndex].imgUrl}
-    }
-}
-
-// FONCTION DE TIRAGE ALÉATOIRE DANS LE DECK - SANS REMISE
-// S'il reste des cartes dans le deck, newRandomCardUrl renvoie une carte, sinon, la fonction renvoie null
-function drawRandomCardNoDouble(cardsArray){
-
+    // Si le deck comporte encore des cartes
     if(cardsArray.length>0){
         //Générer un index aléatoire dans les limites du nombre de cartes du deck
         let nbOfCards = cardsArray.length;
         let randomIndex = Math.floor(Math.random() * nbOfCards);
-        console.log("randomIndex :", randomIndex);
-        
-        let newRandomCard = cardsArray[randomIndex];
-        console.log("newRandomCard: ", newRandomCard);
 
+        // Tirer la carte à l'index généré
+        let newRandomCard = cardsArray[randomIndex];
+
+        // Défausser la carte en la retirant du tableau
         cardsArray.splice(randomIndex, 1);
-        console.log("cardsArray après suppression : ", cardsArray);
         return newRandomCard;
     }else{
+        // Si le deck est vide, on ne renvoie rien
         return null;
     }
+}
 
+// Tirage aléatoire à la barre d'espace
+function drawRandomCardOnSpaceBar(event){
+    if (event.key == " " ||
+        event.code == "Space" ||      
+        event.keyCode == 32      
+    ) {
+        console.log("spacebar !");
+    }else{
+        console.log("other key !");
+    }
 }
